b8b8d60f35f7d93d2dc3b00a5ee7fc0fec3d91f0
Oleh Prypin <blaxpirit@gmail.com>
2015-08-28 14:47:19 +0300

    Initial commit

---
+++ book/ungrey.js
@@ -0,0 +1,13 @@
+require(["jQuery"], function($) {
+    $(function() {
+        // Find summary items that have an inactive title (span) and subitems (ul)
+        $('ul.summary li>span+ul').prev().css({
+            // Make the span fully opaque and show as clickable
+            opacity: 1,
+            cursor: 'pointer'
+        }).click(function() {
+            // When the span is clicked, click the first link inside this chapter instead
+            $(this).parent().find('a').first().click();
+        })
+    });
+});
